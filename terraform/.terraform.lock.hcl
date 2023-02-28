b398f94e5a9d498697d7f7c27be3b9a054b001b5
Nathan Douglas <github@darkdell.net>
2023-02-28 15:20:59 -0500

    Last Terraform commit.

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -1,27 +1,6 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.terraform.io/1password/onepassword" {
-  version     = "1.1.4"
-  constraints = "~> 1.1.4"
-  hashes = [
-    "h1:3TVsJ9fbNOerTE00uFqqKQDW6WRAPP0kZVhVhfd0COw=",
-    "zh:018f3e223ae82ba675f0f088af135a9196326501f9f57f397067a65342cdcca3",
-    "zh:1a077bba8db0ffa0c969bbcd8525d30f4d8018257363408b4e6891b319397e91",
-    "zh:273534ba172eec6bc71cdf107cb51197fb58818b5a31bf9812b89ebaa0545522",
-    "zh:2956acd7594b024f708588fe47f495abb555593bd0360c544b97a700490810fb",
-    "zh:2a204b457c53c2808a990e9fe2ceafc73c1371eb11d60a7e9baf5f2db2d5891d",
-    "zh:329bcfccbb6f96d3cdfc79d04c677545a5e170e3a29480a3002459fc1801db0e",
-    "zh:446e6e9fad793e14f53cecf8140407fab6a8178590a032f764a0fe300171e2d8",
-    "zh:69ca3fa115f292b48defdc5c06297f68ce38389596b0cb715ac8730a1080512d",
-    "zh:cce0942774602a0357251f3c10570fb71c4d2b4769c324cea6532e75d7e58c14",
-    "zh:d4aec090dbd03beba2f4eea52d64cfc9417433418c7ada6d76649364429af220",
-    "zh:e99adab86730b3847332d93d66621fdf4fea3b309579266c45469358f68f10c9",
-    "zh:eef501df7d4f1694cb1145a6c8a905d7338a4abd0001d43e2425986e3d8768f6",
-    "zh:f8424c10cc66fd8feea457aaab47fc122f67ca5a9dfbcb47c7c79859cf7291ed",
-  ]
-}
-
 provider "registry.terraform.io/hashicorp/external" {
   version = "2.2.3"
   hashes = [
@@ -38,6 +17,44 @@ provider "registry.terraform.io/hashicorp/external" {
     "zh:b763efdd69fd097616b4a4c89cf333b4cee9699ac6432d73d2756f8335d1213f",
     "zh:e3b561efdee510b2b445f76a52a902c52bee8e13095e7f4bed7c80f10f8d294a",
     "zh:fe660bb8781ee043a093b9a20e53069974475dcaa5791a1f45fd03c61a26478a",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.3.0"
+  hashes = [
+    "h1:U+DbBqKnXSIqC2z7qIko2dy8w6wwuZd89orPvfeqHk0=",
+    "zh:1f1920b3f78c31c6b69cdfe1e016a959667c0e2d01934e1a084b94d5a02cd9d2",
+    "zh:550a3cdae0ddb350942624e7b2e8b31d28bc15c20511553432413b1f38f4b214",
+    "zh:68d1d9ccbfce2ce56b28a23b22833a5369d4c719d6d75d50e101a8a8dbe33b9b",
+    "zh:6ae3ad6d865a906920c313ec2f413d080efe32c230aca711fd106b4cb9022ced",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:a0f413d50f54124057ae3dcd9353a797b84e91dc34bcf85c34a06f8aef1f9b12",
+    "zh:a2ac6d4088ceddcd73d88505e18b8226a6e008bff967b9e2d04254ef71b4ac6b",
+    "zh:a851010672e5218bdd4c4ea1822706c9025ef813a03da716d647dd6f8e2cffb0",
+    "zh:aa797561755041ef2fad99ee9ffc12b5e724e246bb019b21d7409afc2ece3232",
+    "zh:c6afa960a20d776f54bb1fc260cd13ead17280ebd87f05b9abcaa841ed29d289",
+    "zh:df0975e86b30bb89717b8c8d6d4690b21db66de06e79e6d6cfda769f3304afe6",
+    "zh:f0d3cc3da72135efdbe8f4cfbfb0f2f7174827887990a5545e6db1981f0d3a7c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version = "3.2.1"
+  hashes = [
+    "h1:ydA0/SNRVB1o95btfshvYsmxA+jZFRZcvKzZSB+4S1M=",
+    "zh:58ed64389620cc7b82f01332e27723856422820cfd302e304b5f6c3436fb9840",
+    "zh:62a5cc82c3b2ddef7ef3a6f2fedb7b9b3deff4ab7b414938b08e51d6e8be87cb",
+    "zh:63cff4de03af983175a7e37e52d4bd89d990be256b16b5c7f919aff5ad485aa5",
+    "zh:74cb22c6700e48486b7cabefa10b33b801dfcab56f1a6ac9b6624531f3d36ea3",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:79e553aff77f1cfa9012a2218b8238dd672ea5e1b2924775ac9ac24d2a75c238",
+    "zh:a1e06ddda0b5ac48f7e7c7d59e1ab5a4073bbcf876c73c0299e4610ed53859dc",
+    "zh:c37a97090f1a82222925d45d84483b2aa702ef7ab66532af6cbcfb567818b970",
+    "zh:e4453fbebf90c53ca3323a92e7ca0f9961427d2f0ce0d2b65523cc04d5d999c2",
+    "zh:e80a746921946d8b6761e77305b752ad188da60688cfd2059322875d363be5f5",
+    "zh:fbdb892d9822ed0e4cb60f2fedbdbb556e4da0d88d3b942ae963ed6ff091e48f",
+    "zh:fca01a623d90d0cad0843102f9b8b9fe0d3ff8244593bd817f126582b52dd694",
   ]
 }
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,71 @@
 terraform {
   required_providers {
-    onepassword = {
-      source  = "1Password/onepassword"
-      version = "~> 1.1.4"
-    }
     proxmox = {
-      source  = "telmate/proxmox"
+      source = "telmate/proxmox"
       version = "2.9.13"
     }
   }
   required_version = ">= 1.3.8"
 }
 
-variable "onepassword_vault_id" {
+# Proxmox host, just because I get tired of typing it.
+variable "proxmox_host" {
   type = string
 }
 
-variable "onepassword_api_token" {
-  type      = string
-  sensitive = true
-}
-
+# API username for Lefford Proxmox VE server.
 variable "proxmox_api_username" {
   type = string
 }
 
+# API password for Lefford Proxmox VE server.
 variable "proxmox_api_password" {
   type      = string
   sensitive = true
 }
 
+# SSH username for Lefford Proxmox VE server.
+variable "proxmox_ssh_username" {
+  type = string
+}
+
+# SSH password for Lefford Proxmox VE server.
+variable "proxmox_ssh_password" {
+  type      = string
+  sensitive = true
+}
+
+# SSH public key for client, uploaded to each host.
 variable "client_ssh_pub_key" {
   type      = string
   sensitive = false
 }
 
-provider "onepassword" {
-  url   = "https://1password-connect-api.lefford.hellholt.net"
-  token = var.onepassword_api_token
-}
-
+# Configure the Proxmox VE provider.
 provider "proxmox" {
   pm_user     = var.proxmox_api_username
   pm_password = var.proxmox_api_password
   pm_api_url  = "https://pve.lefford.hellholt.net/api2/json"
   pm_debug    = true
-
 }
 
+# A Resource to force recreation of other resources.
+resource "null_resource" "always_run" {
+  triggers = {
+    timestamp = "${timestamp()}"
+  }
+}
+
+# Collect Ansible inventory data.
+#
+# This is a bit of a hack, but it works. The script collects the data from the
+# Ansible inventory and writes it to a temporary file. The temporary file is
+# then read by the data "external" resource and the data is passed to the
+# Terraform local variables.
 data "external" "ansible_inventory" {
   program = ["python3", "${path.module}/scripts/collect_ansible_inventory.py"]
+  # The script expects the following arguments, which are passed in via stdin
+  # as a JSON object.
   query = {
     inventory_file = "${path.module}/../ansible/inventory/hosts"
     host_vars_dir  = "${path.module}/../ansible/inventory/host_vars/"
@@ -57,31 +73,179 @@ data "external" "ansible_inventory" {
   }
 }
 
+# Local variable definition.
 locals {
+  # Retrieve the merged Ansible inventory data.
   ansible_inventory = jsondecode(data.external.ansible_inventory.result.result)
-  pve_lxc_hosts     = local.ansible_inventory.all.children.pve_lxc.hosts
+  # All hosts
+  pve_hosts = local.ansible_inventory.all.children.pve_hosts.hosts
+  # LXC hosts
+  pve_lxc_hosts      = local.ansible_inventory.all.children.pve_lxc.hosts
   pve_lxc_host_names = toset(keys(local.pve_lxc_hosts))
-  // pve_k8s_cp_hosts = [yamldecode(data.yaml_decode.merged_inventory).all.children.pve_k8s.children.pve_k8s_cp.hosts]
-  // pve_k8s_worker_hosts = [yamldecode(data.yaml_decode.merged_inventory).all.children.pve_k8s.children.pve_k8s_workers.hosts]
-  // pve_kvm_hosts = [yamldecode(data.yaml_decode.merged_inventory).all.children.pve_kvm.hosts]
-  // host_data = yamldecode(data.yaml_decode.merged_inventory).all.children.pve_hosts.hosts
+  # K8s control plane hosts
+  pve_k8s_cp_hosts      = local.ansible_inventory.all.children.pve_k8s.children.pve_k8s_cp.hosts
+  pve_k8s_cp_host_names = toset(keys(local.pve_k8s_cp_hosts))
+  # K8s worker hosts
+  pve_k8s_worker_hosts      = local.ansible_inventory.all.children.pve_k8s.children.pve_k8s_workers.hosts
+  pve_k8s_worker_host_names = toset(keys(local.pve_k8s_worker_hosts))
+  # Miscellaneous KVM hosts
+  pve_kvm_hosts      = local.ansible_inventory.all.children.pve_kvm.hosts
+  pve_kvm_host_names = toset(keys(local.pve_kvm_hosts))
 }
 
+# Create a local copy of the files for transfer to Proxmox.
+resource "local_file" "pve_k8s_cp_cloud_init" {
+  for_each = toset(local.pve_k8s_cp_host_names)
+  # Replace the variables in the cloud-config template.
+  content = templatefile("${path.module}/files/pve_k8s_cloud_init.cloud_config", {
+    ssh_key  = var.client_ssh_pub_key
+    hostname = each.key
+    domain   = "${var.proxmox_host}.hellholt.net"
+  })
+  filename = "${path.module}/files/tmp/pve_k8s_cp_cloud_init__${each.key}.cfg"
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run
+    ]
+  }
+}
+
+# Create a local copy of the files for transfer to Proxmox.
+resource "local_file" "pve_k8s_worker_cloud_init" {
+  for_each = toset(local.pve_k8s_worker_host_names)
+  # Replace the variables in the cloud-config template.
+  content = templatefile("${path.module}/files/pve_k8s_cloud_init.cloud_config", {
+    ssh_key  = var.client_ssh_pub_key
+    hostname = each.key
+    domain   = "${var.proxmox_host}.hellholt.net"
+  })
+  filename = "${path.module}/files/tmp/pve_k8s_worker_cloud_init__${each.key}.cfg"
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run
+    ]
+  }
+}
+
+# Transfer the file to the Proxmox Host.
+resource "null_resource" "pve_k8s_cp_cloud_init" {
+  for_each = toset(local.pve_k8s_cp_host_names)
+  depends_on = [
+    local_file.pve_k8s_cp_cloud_init
+  ]
+  connection {
+    type     = "ssh"
+    user     = var.proxmox_ssh_username
+    password = var.proxmox_ssh_password
+    host     = var.proxmox_host
+  }
+
+  provisioner "file" {
+    source      = local_file.pve_k8s_cp_cloud_init[each.key].filename
+    destination = "/var/lib/vz/snippets/pve_k8s_cp_cloud_init--${each.key}.yml"
+  }
+
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run
+    ]
+  }
+}
+
+# Transfer the file to the Proxmox Host.
+resource "null_resource" "pve_k8s_worker_cloud_init" {
+  for_each = toset(local.pve_k8s_worker_host_names)
+  depends_on = [
+    local_file.pve_k8s_worker_cloud_init
+  ]
+  connection {
+    type     = "ssh"
+    user     = var.proxmox_ssh_username
+    password = var.proxmox_ssh_password
+    host     = var.proxmox_host
+  }
+
+  provisioner "file" {
+    source      = local_file.pve_k8s_worker_cloud_init[each.key].filename
+    destination = "/var/lib/vz/snippets/pve_k8s_worker_cloud_init--${each.key}.yml"
+  }
+
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run
+    ]
+  }
+}
+
+# Create the K8s control plane hosts.
+resource "proxmox_vm_qemu" "pve_k8s_cp_hosts" {
+  for_each = toset(local.pve_k8s_cp_host_names)
+  depends_on = [
+    null_resource.pve_k8s_cp_cloud_init
+  ]
+  name                    = each.key
+  target_node             = var.proxmox_host
+  clone                   = "debian-cloudinit"
+  os_type                 = "cloud-init"
+  cicustom                = "user=local:snippets/pve_k8s_cp_cloud_init--${each.key}.yml"
+  cloudinit_cdrom_storage = "leo"
+  vmid                    = local.pve_hosts[each.key].pve_vm_id
+  bios                    = "seabios"
+  onboot                  = true
+  agent                   = 1
+  memory                  = local.pve_hosts[each.key].pve_memory
+  cores                   = local.pve_hosts[each.key].pve_cores
+  scsihw                  = "virtio-scsi-pci"
+  network {
+    model   = "virtio"
+    macaddr = local.pve_hosts[each.key].pve_mac_address
+    bridge  = "vmbr0"
+  }
+}
+
+# Create the K8s worker VMs.
+resource "proxmox_vm_qemu" "pve_k8s_worker_hosts" {
+  for_each = toset(local.pve_k8s_worker_host_names)
+  depends_on = [
+    null_resource.pve_k8s_worker_cloud_init,
+    proxmox_vm_qemu.pve_k8s_cp_hosts
+  ]
+  name                    = each.key
+  target_node             = var.proxmox_host
+  clone                   = "debian-cloudinit"
+  os_type                 = "cloud-init"
+  cicustom                = "user=local:snippets/pve_k8s_worker_cloud_init--${each.key}.yml"
+  cloudinit_cdrom_storage = "leo"
+  vmid                    = local.pve_hosts[each.key].pve_vm_id
+  bios                    = "seabios"
+  onboot                  = true
+  agent                   = 1
+  memory                  = local.pve_hosts[each.key].pve_memory
+  cores                   = local.pve_hosts[each.key].pve_cores
+  scsihw                  = "virtio-scsi-pci"
+  network {
+    model   = "virtio"
+    macaddr = local.pve_hosts[each.key].pve_mac_address
+    bridge  = "vmbr0"
+  }
+}
+
+# Define the LXC hosts using the local variables.
 resource "proxmox_lxc" "lxc_hosts" {
   for_each        = toset(local.pve_lxc_host_names)
-  target_node     = "lefford"
+  target_node     = var.proxmox_host
   hostname        = each.key
-  ostemplate      = local.pve_lxc_hosts[each.key].pve_lxc_template
-  unprivileged    = local.pve_lxc_hosts[each.key].pve_option_unprivileged
-  onboot          = local.pve_lxc_hosts[each.key].pve_option_onboot
-  start           = local.pve_lxc_hosts[each.key].pve_option_start
-  swap            = local.pve_lxc_hosts[each.key].pve_swap
-  memory          = local.pve_lxc_hosts[each.key].pve_memory
+  ostemplate      = local.pve_hosts[each.key].pve_lxc_template
+  unprivileged    = local.pve_hosts[each.key].pve_option_unprivileged
+  onboot          = local.pve_hosts[each.key].pve_option_onboot
+  start           = local.pve_hosts[each.key].pve_option_start
+  swap            = local.pve_hosts[each.key].pve_swap
+  memory          = local.pve_hosts[each.key].pve_memory
   password        = var.proxmox_api_password
   ssh_public_keys = <<-EOT
     ${var.client_ssh_pub_key}
   EOT
-  vmid            = local.pve_lxc_hosts[each.key].pve_vm_id
+  vmid            = local.pve_hosts[each.key].pve_vm_id
 
   features {
     fuse    = true
@@ -91,16 +255,15 @@ resource "proxmox_lxc" "lxc_hosts" {
 
   rootfs {
     storage = "leo"
-    size    = local.pve_lxc_hosts[each.key].pve_rootfs_size
+    size    = local.pve_hosts[each.key].pve_rootfs_size
   }
 
   network {
     name   = "eth0"
     bridge = "vmbr0"
-    hwaddr = local.pve_lxc_hosts[each.key].pve_mac_address
+    hwaddr = local.pve_hosts[each.key].pve_mac_address
     ip     = "dhcp"
     ip6    = "auto"
   }
 
 }
-
